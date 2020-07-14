8f3a0cde7fb7b8fb5fa29db8285231c632648220
rssj <674878857@qq.com>
2020-07-14 19:09:52 +0800

    首页开发

--- wanandroid/src/main/java/com/chlhrssj/wanandroid/ui/main/MainFragmentPagerAdapter.kt
+++ wanandroid/src/main/java/com/chlhrssj/wanandroid/ui/main/MainFragmentPagerAdapter.kt
@@ -1,18 +1,23 @@
 package com.chlhrssj.wanandroid.ui.main
 
+import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 /**
  * Create by rssj on 2020/7/13
  */
-class MainFragmentPagerAdapter : FragmentStateAdapter() {
+class MainFragmentPagerAdapter(@NonNull var fm : FragmentManager, @NonNull var lifecycle: Lifecycle) : FragmentStateAdapter(fm, lifecycle) {
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return fm.fragments.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return fm.getFragment(position)
     }
+
+
 }
