2e6ae2ee45c5a4512ae2819fdbca041d0077b811
rssj <674878857@qq.com>
2020-09-28 19:28:07 +0800

    banner

---
+++ banner/src/main/java/com/rssj/banner/BannerView.kt
@@ -0,0 +1,22 @@
+package com.rssj.banner
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+/**
+ * Create by rssj on 2020/9/28
+ */
+class BannerView : RelativeLayout {
+
+    constructor(context: Context) : super(context) {}
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {}
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attributeSet,
+        defStyleAttr
+    ) {}
+
+}
