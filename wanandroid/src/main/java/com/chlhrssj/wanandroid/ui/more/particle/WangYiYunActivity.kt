c6b175d2eae88ddb15717eee49b0820a0ce716ce
rssj <674878857@qq.com>
2020-10-26 18:33:01 +0800

    mark

--- wanandroid/src/main/java/com/chlhrssj/wanandroid/ui/more/particle/WangYiYunActivity.kt
+++ wanandroid/src/main/java/com/chlhrssj/wanandroid/ui/more/particle/WangYiYunActivity.kt
@@ -2,6 +2,7 @@ package com.chlhrssj.wanandroid.ui.more.particle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.chlhrssj.basecore.base.ui.mvc.BaseVcActivity
 import com.chlhrssj.wanandroid.databinding.ActivityWangYiYunBinding
 
@@ -9,6 +10,13 @@ class WangYiYunActivity : BaseVcActivity<ActivityWangYiYunBinding>() {
 
     override fun initBinding(): ActivityWangYiYunBinding {
         return ActivityWangYiYunBinding.inflate(layoutInflater)
+    }
+
+    override fun initView() {
+        super.initView()
+
+
+
     }
 
 }
